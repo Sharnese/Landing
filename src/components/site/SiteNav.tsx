6838855affbbe18786e5675b043ca096e351b2b8
Digital Jones <jmsharnese@gmail.com>
2026-08-02 10:33:46 -0400

    Fix typo in SiteNav component name

--- src/components/site/SiteNav.tsx
+++ src/components/site/SiteNav.tsx
@@ -18,7 +18,7 @@ const Logo = () => (
         <path d="M9 11l3 3L22 4" /><path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11" />
       </svg>
     </div>
-    <span>MyHcbs<span className="text-[#116AEF]"></span></span>
+    <span>MyHCBS<span className="text-[#116AEF]"></span></span>
   </a>
 );
 
